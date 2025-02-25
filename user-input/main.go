780493d22be2d01f666764a1d5c919bc98015565
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-26 01:49:34 +0530

    Input

--- user-input/main.go
+++ user-input/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main(){
-	
+	fmt.Println("What is your name?");
+	var name string;
+	fmt.Scan(&name); // Scan reads the input from the user till the first whitespace and stores it in the variable
+	fmt.Println("Hello,", name);
+
+	fmt.Println("What is your full name?");
+	reader := bufio.NewReader(os.Stdin); // Create a reader object of buffer reader to read input from the user
+	fullName, _ := reader.ReadString('\n'); // Read till '\n' is encountered
+	fmt.Println(fullName)
 }
