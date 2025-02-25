a7354a20ad503a11f8d928095983091601b3acb0
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-26 00:17:33 +0530

    Getting Started

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
-package main
+package main // Every Go program must start with a package declaration. The main package is used to create an executable file.
+// An executable program must contain a main package.
 
-import "fmt"
+import "fmt" // fmt is a standard library package that provides formatted I/O functions.
+// Go uses relative imports to bring packages into the file.
 
 func main(){
 	fmt.Println("Hello World")
