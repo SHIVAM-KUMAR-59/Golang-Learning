90ec933d2aaa2c9225d0c0781ee89b70e05a369f
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-03-08 15:54:53 +0530

    Web Requests

--- 19. web-requests/main.go
+++ 19. web-requests/main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
+	"fmt"       // Package for formatted I/O operations
+	"io/ioutil" // Package for reading from response body
+	"net/http"  // Package for making HTTP requests
 )
 
-func main(){
+func main() {
 	fmt.Println("Learning Web Requests in GoLang")
 
+	// Making a GET request to fetch data from the given URL
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
-	if err != nil{
-		fmt.Println("Error getting data", err)
-		return
+	if err != nil { // Checking if there was an error in making the request
+		fmt.Println("Error getting data:", err)
+		return // Exit the function if an error occurs
 	}
+	// Ensure that the response body is closed once the function exits
 	defer res.Body.Close()
 
+	// Reading the response body into a byte slice
 	data, ioErr := ioutil.ReadAll(res.Body)
-	if(ioErr) != nil{
-		fmt.Println("Error while reading response", ioErr)
-		return
+	if ioErr != nil { // Checking for errors while reading response data
+		fmt.Println("Error while reading response:", ioErr)
+		return // Exit the function if an error occurs
 	}
 
-	fmt.Println("Resoponse is:", string(data))
+	// Converting the byte slice to a string and printing the response
+	fmt.Println("Response is:", string(data))
 }
